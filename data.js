b6a727573e700dbff9beac1f915d74b6114c68db
uz-zi <uzmanzahid6655@gmail.com>
2025-06-22 01:14:49 +0100

    boom

---
+++ data.js
@@ -0,0 +1,32 @@
+// data.js
+
+const products = [
+  {
+    id: 1,
+    name: "Men's Workout Tank Top",
+    price: "€12.50",
+    image: "https://m.media-amazon.com/images/I/71ZKjliK6xL._AC_UL1500_.jpg", // From Amazon Muscle Cmdr tank :contentReference[oaicite:1]{index=1}
+    category: "Men",
+  },
+  {
+    id: 2,
+    name: "Women's High-Waisted Leggings",
+    price: "€55.00",
+    image: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/96b7f34e-c2da-4c4d-a2a6-1b1b211b290e/pro-sculpt-7-8-leggings-WyMy5Q.png", // From Nike IE Pro Sculpt :contentReference[oaicite:2]{index=2}
+    category: "Women",
+  },
+  {
+    id: 3,
+    name: "Motion Seamless Leggings",
+    price: "€45.00",
+    image: "https://cdn.womensbest.com/media/catalog/product/cache/2ee9ee6a3f8ac9cea3f118fbd39f55d3/p/o/power-seamless-leggings-black_1.jpg", // From Women's Best Seamless :contentReference[oaicite:3]{index=3}
+    category: "Women",
+  },
+  {
+    id: 4,
+    name: "Men's Breathable Fitness Tank",
+    price: "€9.00",
+    image: "https://contents.mediadecathlon.com/p1963850/89f39da2c19001d775e84b598a09552b/p1963850.jpg", // From Decathlon tank :contentReference[oaicite:4]{index=4}
+    category: "Men",
+  }
+];
